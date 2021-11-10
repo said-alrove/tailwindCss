5db1abd0b9ade7df09c80046d0230bf15c24dac5
Said Rosas <saidmift@outlook.es>
2021-11-10 13:07:25 -0600

    Add the Tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    // *NOTE:* This way everytime postcss compile the stylesheet will only load the used classes (this isn't recommend unless you're done with the project and want to send it to production, this is because otherwise, having to compile the files everytime you've added new classes to your project is tedious).
+    // enabled: true,
+    // content: ['./*.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
