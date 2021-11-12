e8c3cb8114c420189df778bac569bec349bd0aa3
Said Rosas <saidmift@outlook.es>
2021-11-12 12:28:06 -0600

    Allows Purge option
    The Purge Css option was allowed in order
    of having a lighter output file for production.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   purge: {
     // *NOTE:* This way everytime postcss compile the stylesheet will only load the used classes (this isn't recommend unless you're done with the project and want to send it to production, this is because otherwise, having to compile the files everytime you've added new classes to your project is tedious).
-    // enabled: true,
-    // content: ['./*.html']
+    enabled: true,
+    content: ['./*.html']
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
