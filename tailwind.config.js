5ffa99479f03fb5310649b9a2a7fd357a2a2d71a
Said Rosas <saidmift@outlook.es>
2021-11-19 19:46:51 -0600

    Set to false the enable key on the purge array

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     // *NOTE:* This way everytime postcss compile the stylesheet will only load the used classes (this isn't recommend unless you're done with the project and want to send it to production, this is because otherwise, having to compile the files everytime you've added new classes to your project is tedious).
-    // enabled: true,
+    enabled: false,
     // content: ['./*.html']
   },
   darkMode: false, // or 'media' or 'class'
